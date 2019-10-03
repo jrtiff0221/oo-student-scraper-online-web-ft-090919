18e6373eb93bb9e00a183e4b82332ccb6864dfd5
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-03 03:04:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,20 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
   def self.scrape_index_page(index_url)
+    html = open(index_url)
     
+    doc = Nokogiri::HTML(html)
+    binding.pry
+  
   end
-
   def self.scrape_profile_page(profile_url)
     
   end
-
 end
 
+  
+  
